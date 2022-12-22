93a35dc5aa45bc6991776206284710522f74509d
asimgunduz <a.gunduz@live.com>
2022-12-22 16:36:10 +0200

    GenPages

---
+++ Generator.Components/Managers/GridManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Force.DeepCloner;
+using Generator.Components.Components;
+using Generator.Components.Interfaces;
+using MudBlazor;
+
+namespace Generator.Components.Managers;
+
+partial class GenGrid: MudTable<object>
+{
+    
+
+    public void Create()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Delete()
+    {
+
+    }
+
+    public void Load()
+    {
+
+    }
+
+    public void Cancel()
+    {
+
+    }
+
+    public void BackUp(object element)
+    {
+       
+    }
+
+
+    public void OnCommit()
+    {
+
+    }
+
+}
+

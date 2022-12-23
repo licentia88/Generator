39338a2fced7e57125dc08c8c6e9db9d1b0bcfd4
asimgunduz <a.gunduz@live.com>
2022-12-23 08:30:15 +0200

    added

---
+++ Generator.Components/Partials/GridManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Force.DeepCloner;
+using Generator.Components.Components;
+using Generator.Components.Interfaces;
+using MudBlazor;
+
+namespace Generator.Components.Partials;
+
+
+

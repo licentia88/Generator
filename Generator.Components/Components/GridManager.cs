8d37873ae3aa8df6968897634b0f0ad890d0ec3d
asimgunduz <a.gunduz@live.com>
2023-01-05 12:02:04 +0200

    lt

---
+++ Generator.Components/Components/GridManager.cs
@@ -0,0 +1,49 @@
+﻿using Generator.Components.Enums;
+using Generator.Shared.Extensions;
+using Mapster;
+
+namespace Generator.Components.Components;
+
+public class GridManager
+{
+    public GenGrid Grid { get; }
+
+    public GridManager(GenGrid grid)
+    {
+        Grid = grid;
+    }
+
+    public async Task Create()
+    {
+        Grid.EditButtonActionList.Clear();
+
+        Grid.ViewState = ViewState.Create;
+
+        var datasourceModelType = Grid.DataSource.GetType().GenericTypeArguments[0];
+
+        var newData = Grid.Components.Where(x => x is not GenSpacer).ToDictionary(comp => comp.BindingField, comp => comp.GetDefaultValue);
+
+        var adaptedData = newData.Adapt(typeof(Dictionary<string, object>), datasourceModelType);
+
+        Grid.SelectedItem = adaptedData;
+
+        if (Grid.EditMode == EditMode.Inline)
+        {
+            Grid.DataSource.Insert(0, Grid.SelectedItem);
+
+            return;
+        }
+
+
+        await Grid.ShowDialogAsync<GenPage>();
+
+    }
+
+    public async Task Edit()
+    {
+        Grid.ViewState = ViewState.Update;
+
+        await Grid.ShowDialogAsync<GenPage>();
+
+    }
+}

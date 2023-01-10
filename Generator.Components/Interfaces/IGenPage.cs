9930972e5305e69aae4a2649847d413c685531b3
asimgunduz <a.gunduz@live.com>
2023-01-10 19:25:59 +0200

    latest

---
+++ Generator.Components/Interfaces/IGenPage.cs
@@ -0,0 +1,15 @@
+﻿using Generator.Components.Components;
+using MudBlazor;
+
+namespace Generator.Components.Interfaces;
+
+public interface IGenPage<TModel> : IGenView where TModel:new()
+{
+    public MudDialogInstance MudDialog { get; set; }
+
+    public TModel ViewModel { get; set; }
+
+    public bool EnableModelValidation { get; set; }
+
+    public GenGrid<TModel> GenGrid { get; set; }
+}

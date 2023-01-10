6fc2f92e1292933f37bf748874009f58b00ed423
asimgunduz <a.gunduz@live.com>
2023-01-10 19:25:59 +0200

    latest

---
+++ Generator.Components/Interfaces/IGenView.cs
@@ -0,0 +1,28 @@
+﻿using Generator.Components.Args;
+using Generator.Components.Enums;
+using Microsoft.AspNetCore.Components;
+
+namespace Generator.Components.Interfaces;
+
+public interface IGenView : IGenCompRenderer
+{
+    public string Title { get; set; }
+
+    public ViewState ViewState { get; set; }
+
+    public EditMode EditMode { get; set; }
+
+    public object OriginalEditItem { get; set; }
+
+    public EventCallback<GenGridArgs> Create { get; set; }
+
+    public EventCallback<GenGridArgs> Update { get; set; }
+
+    public EventCallback<GenGridArgs> Delete { get; set; }
+
+    public EventCallback<GenGridArgs> Cancel { get; set; }
+
+    public List<IGenComponent> Components { get; set; }
+
+    public TComponent GetComponent<TComponent>(string bindingField) where TComponent : IGenComponent;
+}

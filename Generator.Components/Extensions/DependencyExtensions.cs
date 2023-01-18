f0b50a1059b70f19e5ea9c6c6858754dd88e9b8d
asimgunduz <a.gunduz@live.com>
2023-01-18 14:40:05 +0200

    lt

---
+++ Generator.Components/Extensions/DependencyExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Generator.Components.Validators;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Generator.Components.Extensions
+{
+    public static class DependencyExtensions
+    {
+        public static void RegisterComponents(this IServiceCollection Services)
+        {
+            Services.AddScoped(typeof(GenValidator<>));
+        }
+    }
+}
+

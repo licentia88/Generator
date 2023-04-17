59ce3aa16f44ccdd71c875fc3752c4146d7ba0d9
asimgunduz <a.gunduz@live.com>
2023-04-17 18:09:12 +0300

    Need to work on generator ui

--- Generator.Server/Services/PagesDService.cs
+++ Generator.Server/Services/PagesDService.cs
@@ -7,89 +7,89 @@ using ProtoBuf.Grpc;
 
 namespace Generator.Server.Services;
 
-public class PagesDService : GenericServiceBase<GeneratorContext, PAGES_D>, IPagesDService
-{
-    public PagesDService(IServiceProvider provider) : base(provider)
-    {
-    }
+//public class PagesDService : GenericServiceBase<GeneratorContext, PAGES_D>, IPagesDService
+//{
+//    public PagesDService(IServiceProvider provider) : base(provider)
+//    {
+//    }
 
-    public new Task<RESPONSE_RESULT<PAGES_D>> CreateAsync(RESPONSE_REQUEST<PAGES_D> request, CallContext context = default)
-    {
-        return TaskHandler.ExecuteModelAsync(async () =>
-        {
-            var page = request.RR_DATA;
+//    public new Task<RESPONSE_RESULT<PAGES_D>> CreateAsync(RESPONSE_REQUEST<PAGES_D> request, CallContext context = default)
+//    {
+//        return TaskHandler.ExecuteModelAsync(async () =>
+//        {
+//            var page = request.RR_DATA;
 
-            //Note:CRUD icin button eklemeye gerek yok.
-            if (page.PM_CREATE)
-            {
-                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Create.ToString(), page.CB_CODE);
-                page.PERMISSIONS.Add(newPermission);
-            };
+//            //Note:CRUD icin button eklemeye gerek yok.
+//            if (page.PM_CREATE)
+//            {
+//                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Create.ToString(), page.CB_CODE);
+//                page.PERMISSIONS.Add(newPermission);
+//            };
 
-            if (page.PM_READ)
-            {
-                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Read.ToString(), page.CB_CODE);
-                page.PERMISSIONS.Add(newPermission);
-            };
+//            if (page.PM_READ)
+//            {
+//                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Read.ToString(), page.CB_CODE);
+//                page.PERMISSIONS.Add(newPermission);
+//            };
 
-            if (page.PM_UPDATE)
-            {
-                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Update.ToString(), page.CB_CODE);
-                page.PERMISSIONS.Add(newPermission);
-            };
+//            if (page.PM_UPDATE)
+//            {
+//                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Update.ToString(), page.CB_CODE);
+//                page.PERMISSIONS.Add(newPermission);
+//            };
 
-            if (page.PM_DELETE)
-            {
-                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Delete.ToString(), page.CB_CODE);
-                page.PERMISSIONS.Add(newPermission);
-            };
+//            if (page.PM_DELETE)
+//            {
+//                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Delete.ToString(), page.CB_CODE);
+//                page.PERMISSIONS.Add(newPermission);
+//            };
 
 
-            Db.PAGES_D.Add(page);
+//            Db.PAGES_D.Add(page);
 
-            await Db.SaveChangesAsync();
+//            await Db.SaveChangesAsync();
 
-            return page;
-        });
-    }
+//            return page;
+//        });
+//    }
 
-    public new Task<RESPONSE_RESULT<PAGES_D>> UpdateAsync(RESPONSE_REQUEST<PAGES_D> request, CallContext context = default)
-    {
-        return TaskHandler.ExecuteModelAsync(async () =>
-        {
-            var page = request.RR_DATA;
+//    public new Task<RESPONSE_RESULT<PAGES_D>> UpdateAsync(RESPONSE_REQUEST<PAGES_D> request, CallContext context = default)
+//    {
+//        return TaskHandler.ExecuteModelAsync(async () =>
+//        {
+//            var page = request.RR_DATA;
 
-            var existingPermissions = page.PERMISSIONS;
+//            var existingPermissions = page.PERMISSIONS;
 
-            PERMISSIONS existingCreatePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Create.ToString()));
-            PERMISSIONS existingUpdatePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Update.ToString()));
-            PERMISSIONS existingDeletePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Delete.ToString()));
+//            PERMISSIONS existingCreatePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Create.ToString()));
+//            PERMISSIONS existingUpdatePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Update.ToString()));
+//            PERMISSIONS existingDeletePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Delete.ToString()));
 
-            if (!page.PM_CREATE)
-            {
-                page.PERMISSIONS.Remove(existingCreatePermission);
-            }
+//            if (!page.PM_CREATE)
+//            {
+//                page.PERMISSIONS.Remove(existingCreatePermission);
+//            }
              
-            if (!page.PM_UPDATE)
-            {
-                page.PERMISSIONS.Remove(existingUpdatePermission);
-            }
+//            if (!page.PM_UPDATE)
+//            {
+//                page.PERMISSIONS.Remove(existingUpdatePermission);
+//            }
             
-            if (!page.PM_DELETE)
-            {
-                page.PERMISSIONS.Remove(existingDeletePermission);
-            }
+//            if (!page.PM_DELETE)
+//            {
+//                page.PERMISSIONS.Remove(existingDeletePermission);
+//            }
 
-            foreach (var existingPermission in existingPermissions)
-            {
-                if (existingPermission.PER_AUTH_CODE != page.CB_CODE)
-                    existingPermission.ChangeAuthCode(page.CB_CODE);
-            }
+//            foreach (var existingPermission in existingPermissions)
+//            {
+//                if (existingPermission.PER_AUTH_CODE != page.CB_CODE)
+//                    existingPermission.ChangeAuthCode(page.CB_CODE);
+//            }
 
-            await Db.SaveChangesAsync();
+//            await Db.SaveChangesAsync();
 
-            return page;
-        });
-    }
+//            return page;
+//        });
+//    }
 
-}
+//}
--- Generator.Server/Services/PagesMService.cs
+++ Generator.Server/Services/PagesMService.cs
@@ -11,91 +11,92 @@ using Microsoft.Extensions.Configuration;
 using ProtoBuf.Grpc;
 
 namespace Generator.Server.Services;
-public class PagesMService : GenericServiceBase<GeneratorContext,PAGES_M>, IPagesMService
-{
-    public PagesMService(IServiceProvider provider) : base(provider)
-    {
-    }
 
-    public new Task<RESPONSE_RESULT<PAGES_M>> CreateAsync(RESPONSE_REQUEST<PAGES_M> request, CallContext context = default)
-    {
-        return TaskHandler.ExecuteModelAsync(async () =>
-        {
-            var page = request.RR_DATA;
+//public class PagesMService : GenericServiceBase<GeneratorContext,PAGES_M>, IPagesMService
+//{
+//    public PagesMService(IServiceProvider provider) : base(provider)
+//    {
+//    }
 
-            //Note:CRUD icin button eklemeye gerek yok.
-            if (page.PM_CREATE)
-            {
-                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Create.ToString(), page.CB_CODE);
-                page.PERMISSIONS.Add(newPermission);
-            };
+//    public new Task<RESPONSE_RESULT<PAGES_M>> CreateAsync(RESPONSE_REQUEST<PAGES_M> request, CallContext context = default)
+//    {
+//        return TaskHandler.ExecuteModelAsync(async () =>
+//        {
+//            var page = request.RR_DATA;
 
-            if (page.PM_READ)
-            {
-                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Read.ToString(), page.CB_CODE);
-                page.PERMISSIONS.Add(newPermission);
-            };
+//            //Note:CRUD icin button eklemeye gerek yok.
+//            if (page.PM_CREATE)
+//            {
+//                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Create.ToString(), page.CB_CODE);
+//                page.PERMISSIONS.Add(newPermission);
+//            };
 
-            if (page.PM_UPDATE)
-            {
-                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Update.ToString(), page.CB_CODE);
-                page.PERMISSIONS.Add(newPermission);
-            };
+//            if (page.PM_READ)
+//            {
+//                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Read.ToString(), page.CB_CODE);
+//                page.PERMISSIONS.Add(newPermission);
+//            };
 
-            if (page.PM_DELETE)
-            {
-                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Delete.ToString(), page.CB_CODE);
-                page.PERMISSIONS.Add(newPermission);
-            };
+//            if (page.PM_UPDATE)
+//            {
+//                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Update.ToString(), page.CB_CODE);
+//                page.PERMISSIONS.Add(newPermission);
+//            };
+
+//            if (page.PM_DELETE)
+//            {
+//                var newPermission = new PERMISSIONS(nameof(CRUD), CRUD.Delete.ToString(), page.CB_CODE);
+//                page.PERMISSIONS.Add(newPermission);
+//            };
 
 
-            Db.PAGES_M.Add(page);
+//            Db.PAGES_M.Add(page);
 
-            await Db.SaveChangesAsync();
+//            await Db.SaveChangesAsync();
 
-            return page;
-        });
-    }
+//            return page;
+//        });
+//    }
 
    
  
 
-    public new Task<RESPONSE_RESULT<PAGES_M>> UpdateAsync(RESPONSE_REQUEST<PAGES_M> request, CallContext context = default)
-    {
-        return TaskHandler.ExecuteModelAsync(async () =>
-        {
-            var page = request.RR_DATA;
+//    public new Task<RESPONSE_RESULT<PAGES_M>> UpdateAsync(RESPONSE_REQUEST<PAGES_M> request, CallContext context = default)
+//    {
+//        return TaskHandler.ExecuteModelAsync(async () =>
+//        {
+//            var page = request.RR_DATA;
 
-            var existingPermissions = page.PERMISSIONS;
+//            var existingPermissions = page.PERMISSIONS;
 
-            PERMISSIONS existingCreatePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Create.ToString()));
-            PERMISSIONS existingUpdatePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Update.ToString()));
-            PERMISSIONS existingDeletePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Delete.ToString()));
+//            PERMISSIONS existingCreatePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Create.ToString()));
+//            PERMISSIONS existingUpdatePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Update.ToString()));
+//            PERMISSIONS existingDeletePermission = existingPermissions.FirstOrDefault(x => x.PER_DESCRIPTION.Equals(CRUD.Delete.ToString()));
 
-            if (!page.PM_CREATE)
-            {
-                page.PERMISSIONS.Remove(existingCreatePermission);
-            }
+//            if (!page.PM_CREATE)
+//            {
+//                page.PERMISSIONS.Remove(existingCreatePermission);
+//            }
 
-            if (!page.PM_UPDATE)
-            {
-                page.PERMISSIONS.Remove(existingUpdatePermission);
-            }
+//            if (!page.PM_UPDATE)
+//            {
+//                page.PERMISSIONS.Remove(existingUpdatePermission);
+//            }
            
-            if (!page.PM_DELETE)
-            {
-                page.PERMISSIONS.Remove(existingDeletePermission);
-            }
+//            if (!page.PM_DELETE)
+//            {
+//                page.PERMISSIONS.Remove(existingDeletePermission);
+//            }
 
-            foreach (var existingPermission in existingPermissions)
-            {
-                existingPermission.ChangeAuthCode(page.CB_CODE);
-            }
+//            foreach (var existingPermission in existingPermissions)
+//            {
+//                existingPermission.ChangeAuthCode(page.CB_CODE);
+//            }
 
-            await Db.SaveChangesAsync();
+//            await Db.SaveChangesAsync();
 
-            return page;
-        });
-    }
+//            return page;
+//        });
+//    }
 
-}
+//}

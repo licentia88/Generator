26e3c4e8d2089ff402938f8da8737953bca7f790
asimgunduz <a.gunduz@live.com>
2023-06-08 11:29:19 +0300

    latest

---
+++ Generator.Server/Services/SeedService.cs
@@ -0,0 +1,24 @@
+﻿using Generator.Server.Helpers;
+using Generator.Server.Services.Base;
+using Generator.Shared.Models.ServiceModels;
+using Generator.Shared.Services;
+using MagicOnion;
+
+namespace Generator.Server.Services;
+
+public class SeedService : MagicBase<ISeedService, IDictionary<string, object>>, ISeedService
+{
+    public SeedService(IServiceProvider provider) : base(provider)
+    {
+    }
+
+    public UnaryResult<RESPONSE_RESULT<List<IDictionary<string, object>>>> FillAsync(string Database, string tableName, params KeyValuePair<string, object>[] where)
+    {
+        return TaskHandler.ExecuteAsync(async () =>
+        {
+            var result = await GetDatabase(Database).QueryAsync($"SELECT * FROM {tableName}", where);
+
+            return result;
+        });
+    }
+}

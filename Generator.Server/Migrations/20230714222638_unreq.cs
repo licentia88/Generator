c1d6e1ecb9b6bede473db264d954ff1b39fa1edd
asimgunduz <a.gunduz@live.com>
2023-07-19 12:17:13 +0300

    additional files

---
+++ Generator.Server/Migrations/20230714222638_unreq.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Generator.Server.Migrations
+{
+    public partial class unreq : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CB_TITLE",
+                table: "COMPONENTS_BASE",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CB_TITLE",
+                table: "COMPONENTS_BASE",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
